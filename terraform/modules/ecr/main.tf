3794554025a29573c1ca50e7521364a6c009c6a4
SohailSajid <sohailsajid79@gmail.com>
2024-11-26 22:46:19 +0000

    update spacing

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,11 +1,14 @@
 resource "aws_ecr_repository" "js_app" {
   name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
+
   image_scanning_configuration {
     scan_on_push = true
   }
-encryption_configuration {
+  
+  encryption_configuration {
     encryption_type = "AES256"
   }
+  
   force_delete = true
 }
