6899bdc1306ab4464dd00594d1241205d2739914
SohailSajid <sohailsajid79@gmail.com>
2024-11-26 22:51:17 +0000

    feat: add Route53 module integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,14 @@ module "aws_ecr_repository" {
   source              = "./modules/ecr"
   ecr_repository_name = "tetris-js-app"
 }
+
+module "route53" {
+  source            = "./modules/route53"
+  zone_name         = "tetris.sohailsajid.dev" # Subdomain
+  cname_record_name = "tetris.sohailsajid.dev"
+  target_dns_name   = var.target_dns_name # Dynamically pass target DNS name
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
