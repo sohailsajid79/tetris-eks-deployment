83fd183672f91f16009dc03ccba9dde743b6691a
SohailSajid <sohailsajid79@gmail.com>
2024-11-27 18:57:22 +0000

    add VPC outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_cidr" {
+  description = "VPC CIDR block"
+  value       = module.vpc.vpc_cidr
+}
+
+output "vpc_id" {
+description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public Subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  description = "Private Subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "nat_gateway_id" {
+  description = "NAT Gateway ID"
+  value       = module.vpc.nat_gateway_id
+}
+
+output "nat_eip" {
+  description = "NAT Gateway Elastic IP"
+  value       = module.vpc.nat_eip
+}
