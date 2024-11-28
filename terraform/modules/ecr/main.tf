18f2d337f3bc1ab2285eac1a7705d0c47f3675d1
SohailSajid <sohailsajid79@gmail.com>
2024-11-28 19:49:10 +0000

    fix: update ecr module

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -5,10 +5,10 @@ resource "aws_ecr_repository" "js_app" {
   image_scanning_configuration {
     scan_on_push = true
   }
-  
+
   encryption_configuration {
     encryption_type = "AES256"
   }
-  
+
   force_delete = true
 }
