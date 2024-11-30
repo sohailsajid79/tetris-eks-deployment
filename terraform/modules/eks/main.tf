000d03f14907b87b7de036ecb460968d837b0dc8
SohailSajid <sohailsajid79@gmail.com>
2024-11-30 11:24:22 +0000

    feat(eks): Refactor EKS module for enhanced functionality - Improved IRSA support with refined configurations - Added new resource definitions for better cluster management

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,25 +1,34 @@
-resource "aws_eks_cluster" "my_cluster" {
-  name     = var.cluster_name
-  role_arn = var.eks_cluster_role
+module "eks" {
+  source                   = "terraform-aws-modules/eks/aws"
+  version                  = "20.30.1"
+  cluster_name             = var.cluster_name
+  cluster_version          = var.cluster_version
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.private_subnets
+  control_plane_subnet_ids = var.public_subnets
+  enable_irsa              = true
+  create_iam_role          = true
+  iam_role_name            = var.iam_role_name
 
-  vpc_config {
-    subnet_ids = var.private_subnets
+
+  eks_managed_node_group_defaults = {
+    desired_capacity = var.node_desired_size
+    min_capacity     = var.node_min_size
+    max_capacity     = var.node_max_size
+    instance_types   = ["t3.medium"]
+    disk_size        = 20
   }
-}
 
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = var.eks_node_role
-  subnet_ids      = var.private_subnets
-
-  scaling_config {
-    desired_size = var.node_desired_size
-    max_size     = var.node_max_size
-    min_size     = var.node_min_size
+  eks_managed_node_groups = {
+    default = {}
   }
 
-  update_config {
-    max_unavailable = var.max_unavailable
+  cluster_encryption_config = {
+    resources = ["secrets"]
   }
+
+  # API endpoint access
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_private_access      = false
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 }
