ae46c6b91f8a0522d31d53ca55a62c4fa986b419
SohailSajid <sohailsajid79@gmail.com>
2024-11-30 11:41:35 +0000

    chore: Update provider configurations - Added support for helm and kubernetes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,9 +4,30 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.6"
+    }
   }
 }
 
 provider "aws" {
   region = "eu-north-1"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
 }
