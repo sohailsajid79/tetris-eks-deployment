6c0bdd466be3b3a0601a4eb68f793606457c9f3e
sohailsajid79 <sohailsajid79@gmail.com>
2024-12-01 10:17:28 +0000

    added dynamodb table for state locking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,20 @@
 terraform {
   backend "s3" {
-    bucket  = "tetris-app-remote-tfstate"
-    key     = "terraform.tfstate"
-    region  = "eu-north-1"
-    encrypt = true
+    bucket         = "tetris-app-remote-tfstate"
+    key            = "terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = "terraform-lock-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
