4096076e6a592b9f07f07c47c745c87c62ff1e9b
SohailSajid <sohailsajid79@gmail.com>
2024-12-01 10:41:33 +0000

    added kubernetes provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,7 +6,11 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">=2.6"
+      version = ">= 2.6"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.13"
     }
   }
 }
@@ -15,19 +19,28 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-data "aws_eks_cluster" "cluster" {
-  name       = module.eks.cluster_name
-  depends_on = [module.eks]
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     exec {
-      api_version = "client.authentication.k8s.io/v1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--region", "eu-north-1", "--cluster-name", var.cluster_name]
       command     = "aws"
     }
   }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
 }
